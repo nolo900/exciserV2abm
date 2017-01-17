e9781cdc885d5feca52b079f0d21c7653d8a1569
Austin Miles <abm@austinbmiles.com>
2017-01-17 13:20:19 -0500

    added mongodb connection string

--- server/app.js
+++ server/app.js
@@ -10,6 +10,23 @@ var users = require('./routes/users');
 
 var app = express();
 
+/////////////////// Connect to database  /////////////////////////////
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/exciser');
+}
+mongoose.connection.on('error', function(err) {
+      console.error('MongoDB connection error: ' + err);
+      process.exit(-1);
+    }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
+///////////////////////////////////////////////////////////////////////
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
